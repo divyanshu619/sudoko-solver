5c160aa2f6e7a96b8d89202abee26d43038b534b
praphul <praphulpatil96@gmail.com>
2020-05-02 21:18:44 +0530

    Added logic to get grid number by index.

--- logic/solve.py
+++ logic/solve.py
@@ -14,8 +14,13 @@ def validate_row(data: np.ndarray):
         print(get_row(data, i))
 
     for i in range(1, 10):
-        get_grid(data, i)
+        get_grid_content(data, i)
 
+    for i in range(0, 9):
+        for j in range(0, 9):
+            print(get_grid_number(i, j), end = "")
+        print()
+    
 
 def get_column(data: np.ndarray, index) -> np.ndarray:
     return data[:, index]
@@ -25,7 +30,30 @@ def get_row(data: np.ndarray, index) -> np.ndarray:
     return data[index, :]
 
 
-def get_grid(data: np.ndarray, grid) -> np.ndarray:
+def get_grid_number(row, col):
+    if row < 3:
+        gridRow = 1
+    elif row < 6:
+        gridRow = 2
+    else:
+        gridRow = 3
+
+    if col < 3:
+        gridCol = 1
+    elif col < 6:
+        gridCol = 2
+    else:
+        gridCol = 3
+
+    if gridRow == 1:
+        return gridCol
+    elif gridRow == 2:
+        return 3+gridCol
+    else:
+        return 6+gridCol    
+
+
+def get_grid_content(data: np.ndarray, grid) -> np.ndarray:
     if not (1 <= grid <= 9):
         raise RuntimeError('Grid number should be between 1 to 9')
 
@@ -41,10 +69,11 @@ def get_grid(data: np.ndarray, grid) -> np.ndarray:
     else:
         grid_row = 3
 
+    grid_content = []
+
     for x in range((grid_row - 1) * 3, (grid_row - 1) * 3 + 3):
         for y in range((grid_col - 1) * 3, (grid_col - 1) * 3 + 3):
-            print(data[x, y], end=" ")
-        print()
+            grid_content.append(data[x, y])
 
 # def get_remaining_digits(data: np.ndarray) -> :
 #
