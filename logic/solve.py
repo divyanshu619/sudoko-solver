560351d46005259ddcb1c8347bc60d7bd0d5f659
praphul <praphulpatil96@gmail.com>
2020-05-02 18:29:02 +0530

    Added logic to print grid content.

--- logic/solve.py
+++ logic/solve.py
@@ -11,8 +11,10 @@ def validate_row(data: np.ndarray):
         print("Column " + str(i))
         print(get_column(data, i))
         print("Row " + str(i))
-        print(get_row(data, i))
+        print(get_row(data, i))        
 
+    for i in range(9):
+        get_grid(data, (0))
 
 def get_column(data: np.ndarray, index) -> np.ndarray:
     return data[:, index]
@@ -20,3 +22,26 @@ def get_column(data: np.ndarray, index) -> np.ndarray:
 
 def get_row(data: np.ndarray, index) -> np.ndarray:
     return data[index, :]
+
+def get_grid(data:np.ndarray, grid):
+
+    if not (1 <= grid <= 9):
+        print("Grid number should be between 1 to 9.")
+        return
+
+    print("Grid #"+str(grid))
+    gridCol = grid % 3
+    if gridCol==0:
+        gridCol=3
+
+    if grid <= 3:
+        gridRow = 1
+    elif grid <=6:
+        gridRow = 2
+    else:
+        gridRow = 3
+
+    for x in range((gridRow-1)*3, (gridRow-1)*3+3):
+        for y in range((gridCol-1)*3, (gridCol-1)*3+3):
+            print(data[x, y], end = " ")
+        print()
