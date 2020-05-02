cdca9659488f6b86a656f3ba14de8f2a24d7e02f
divyanshu619 <divyanshu619@gmail.com>
2020-05-02 15:04:31 +0530

    Added basic dataset and reader

---
+++ logic/solver.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+quizzes = np.zeros((1000000, 81), np.int32)
+solutions = np.zeros((1000000, 81), np.int32)
+for i, line in enumerate(open('sudoku-small.csv', 'r').read().splitlines()[1:]):
+    quiz, solution = line.split(",")
+    for j, q_s in enumerate(zip(quiz, solution)):
+        q, s = q_s
+        quizzes[i, j] = q
+        solutions[i, j] = s
+quizzes = quizzes.reshape((-1, 9, 9))
+solutions = solutions.reshape((-1, 9, 9))
