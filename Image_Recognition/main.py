611634c0c0d4e2819086ccc5f4eb7227b45fd36e
harshgandhi100 <harshgandhi100@gmail.com>
2020-05-03 15:43:46 +0530

    Somewhat working Image Recog

--- Image_Recognition/main.py
+++ Image_Recognition/main.py
@@ -11,40 +11,47 @@ img = cv2.imread("download.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Performing OTSU threshold
-ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+# ret, thresh1 = cv2.threshold(gray, 0, 255,  cv2.THRESH_OTSU | cv2.THRESH_BINARY)
+
+thresh2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
 # Specify structure shape and kernel size.
 # Kernel size increases or decreases the area
 # of the rectangle to be detected.
 # A smaller value like (10, 10) will detect
 # each word instead of a sentence.
-kernel_len = np.array(img).shape[1]//220
+kernel_len = np.array(img).shape[1]//12
 ver_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_len))
 hor_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_len, 1))
 
 rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-image_1 = cv2.erode(thresh1, ver_kernel, iterations=1)
-vertical_lines = cv2.dilate(image_1, ver_kernel, iterations=1)
 
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-image_1 = cv2.erode(thresh1, hor_kernel, iterations=1)
-horizontal_lines = cv2.dilate(image_1, hor_kernel, iterations=1)
+image_1 = cv2.erode(thresh2, ver_kernel, iterations=1)
+vertical_lines = cv2.dilate(image_1, ver_kernel, iterations=6)
+
+image_1 = cv2.erode(thresh2, hor_kernel, iterations=1)
+horizontal_lines = cv2.dilate(image_1, hor_kernel, iterations=6)
+
+img_vh = cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)
+
 
 # Appplying dilation on the threshold image
-dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+
 # rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
-# erosion = cv2.erode(dilation, rect_kernel, iterations=1)
+# erosion = cv2.erode(thresh1, rect_kernel, iterations=1)
+# dilation = cv2.dilate(erosion, rect_kernel, iterations=1)
 # rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 # opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, rect_kernel)
 # Finding contours
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE,
+contours, hierarchy = cv2.findContours(img_vh, cv2.RETR_TREE,
                                        cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.imshow("original", gray)
+cv2.imshow("original", img_vh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # Creating a copy of image
 im2 = img.copy()
+
 
 # A text file is created and flushed
 file = open("recognized.txt", "w+")
@@ -63,15 +70,15 @@ for cnt in contours:
 
     # Cropping the text block for giving input to OCR
     cropped = im2[y:y + h, x:x + w]
-    # cv2.imshow("original", rect)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    cv2.imshow("original", rect)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
     # Open the file in append mode
     file = open("recognized.txt", "a")
 
     # Apply OCR on the cropped image
-    text = pytesseract.image_to_string(cropped)
+    text = pytesseract.image_to_string(cropped,'eng',config='--oem 1  --psm 10 outputbase digits')
 
     # Appending the text into file
     file.write(text)
